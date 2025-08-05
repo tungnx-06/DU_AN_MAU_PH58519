e515dc532e3e658d67868eeaa481161f38c2c40a
Tung Nguyen <ngoxuantung06@gmail.com>
2025-08-05 16:57:27 +0700

    ket noi data

--- mvc-oop-basic-duanmau/commons/env.php
+++ mvc-oop-basic-duanmau/commons/env.php
@@ -3,12 +3,12 @@
 // Biến môi trường, dùng chung toàn hệ thống
 // Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
 
-define('BASE_URL'       , 'http://localhost/duanmau/mvc-oop-basic/');
+define('BASE_URL'       , 'http://localhost/DU_ANMAU/mvc-oop-basic-duanmau/');
 
 define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'database');  // Tên database
+define('DB_NAME'    , 'du_an_mau_ph58519');  // Tên database
 
 define('PATH_ROOT'    , __DIR__ . '/../');
