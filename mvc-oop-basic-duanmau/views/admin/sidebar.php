07137ec369232b294130ceb17af7ff16fb7465d0
Tung Nguyen <ngoxuantung06@gmail.com>
2025-08-22 12:08:45 +0700

    sửa chính tả

--- mvc-oop-basic-duanmau/views/admin/sidebar.php
+++ mvc-oop-basic-duanmau/views/admin/sidebar.php
@@ -1,23 +1,23 @@
 <ul>
     <li>
-        <a href="#">Quan ly danh muc</a>
+        <a href="#">Quản lý danh mục</a>
         <ul>
             <li>
-                <a href="?mode=admin&act=category">Danh Sach Danh Muc</a>
+                <a href="?mode=admin&act=category">Danh Sách Danh Mục</a>
             </li>
             <li>
-                <a href="?mode=admin&act=addcategory">Them Moi Danh Muc</a>
+                <a href="?mode=admin&act=addcategory">Thêm Mới Danh Mục</a>
             </li>
         </ul>
     </li>
     <li>
-        <a href="#">Quan Ly San Pham</a>
+        <a href="#">Quản Lý Sản Phẩm</a>
         <ul>
             <li>
-                <a href="?mode=admin&act=product">Danh Sach San Pham </a>
+                <a href="?mode=admin&act=product">Danh Sách Sản Phẩm </a>
             </li>
             <li>
-                <a href="?mode=admin&act=addproduct">Them moi san pham</a>
+                <a href="?mode=admin&act=addproduct">Thêm Mới Sản Phẩm</a>
             </li>
         </ul>
     </li>
